4ba86a729577b5043ed44b3e8fe36fd353f10f87
Subodh Gujar <subodhsgujar@gmail.com>
2023-06-10 19:18:22 -0500

    Time: 282 ms (11.37%), Space: 18.6 MB (34.30%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -9,7 +9,6 @@ class Solution:
                 break
         i=id0+1
         j=id0
-        print(id0)
         while j > -1 and i < len(nums):
             if nums[j]**2 > nums[i]**2:
                 ans.append(nums[i]**2)
