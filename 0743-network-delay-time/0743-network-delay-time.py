8c36feb6a089d843b9b9ffd7b28335f9375e3a08
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-18 18:03:45 -0500

    Time: 446 ms (97.02%), Space: 18.8 MB (24.44%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,28 +1,29 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         d={}
-        for i in range(n+1):
+        n+=1
+        dist = [float("inf") for _ in range(n)]
+        for i in range(n):
             d[i]=[]
+        h=[]
         for u, v, w in times:
             d[u].append((v, w))
-        
-        dist = [float("inf") for _ in range(n+1)]
-        dist[0] = 0
-        dist[k] = 0
-        h=[]
-        for v, w in d[k]:
-            dist[v]=w
-            heapq.heappush(h, (w, v))
+            if u == k:
+                heapq.heappush(h, (w, v))
+                dist[v]=w
+            
+        dist[0]=0
+        dist[k]=0
         
         while h:
-            w, v = heapq.heappop(h)
-            for vi, wi in d[v]:
-                if (w + wi) < dist[vi]:
-                    dist[vi]=w+wi
-                    heapq.heappush(h, (wi+w, vi))
+            w, u = heapq.heappop(h)
+            for v, wi in d[u]:
+                if wi+w < dist[v]:
+                    dist[v] = wi+w
+                    heapq.heappush(h, (wi+w, v))
         
         ans = max(dist)
-        if ans==float("inf"):
+        if ans == float("inf"):
             return -1
         return ans
         
