dbd488c17bf7895abcedbb8a8e3f6d675d8aece4
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-18 16:39:50 -0500

    Time: 999 ms (91.40%), Space: 68.6 MB (91.77%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -4,19 +4,25 @@ class Solution:
         def dist(p1, p2):
             return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
         n = len(points)
-        t=[]
-        t.append(0)
+        mst = set()
+        mst.add(tuple(points[0]))
+        nonmst = set()
+        for i in range(n):
+            nonmst.add(tuple(points[i]))
+        
         h=[]
+        l=tuple(points[0])
         ans=0
-        while len(t)!=n:
-            for i in range(n):
-                if i in t:
-                    continue
-                d = dist(points[t[-1]], points[i])
-                heapq.heappush(h, (d, t[-1], i))
-            d, i, j = heapq.heappop(h)
-            while h and j in t:
-                d, i ,j = heapq.heappop(h)
-            t.append(j)
+        while len(mst)!=n:
+            for pty in nonmst:
+                d= dist(pty, l)
+                heapq.heappush(h, (d, pty))
+            d, pty = heapq.heappop(h)
+            while pty in mst:
+                d, pty = heapq.heappop(h)
+            mst.add(pty)
+            nonmst.remove(pty)
             ans+=d
+            l=pty
+        
         return ans
