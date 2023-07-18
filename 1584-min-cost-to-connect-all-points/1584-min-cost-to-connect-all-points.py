84bee91cf6012bd2812e1b8995bdbf45e4066a8e
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-18 16:32:27 -0500

    Time: 7359 ms (5.35%), Space: 80.9 MB (84.57%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,33 +1,22 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
-        
 
-        def getDistance(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1]-p2[1])
-        
-        mst = set()
-        nonmst = set()
-        for i in range(1, n):
-            nonmst.add(tuple(points[i]))
-        
-        mst.add(tuple(points[0]))
-        lastpt = tuple(points[0])
-        ans = 0
-        mhp = []
-        while len(mst) != n:
-            for y in nonmst:
-                dist = getDistance(lastpt, y)
-                heapq.heappush(mhp, (dist, lastpt, y))
-                
-            while mhp:
-                mval, ptx, pty = heapq.heappop(mhp)
-                if pty not in mst:
-                    ans+=mval
-                    mst.add(pty)
-                    lastpt = pty
-                    nonmst.remove(pty)
-                    break
-        
+        def dist(p1, p2):
+            return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        n = len(points)
+        t=[]
+        t.append(0)
+        h=[]
+        ans=0
+        while len(t)!=n:
+            for i in range(n):
+                if i in t:
+                    continue
+                d = dist(points[t[-1]], points[i])
+                heapq.heappush(h, (d, t[-1], i))
+            d, i, j = heapq.heappop(h)
+            while h and j in t:
+                d, i ,j = heapq.heappop(h)
+            t.append(j)
+            ans+=d
         return ans
-        
