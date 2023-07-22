06fc3c21389963c39d795a92d11bf829b477c328
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-21 19:38:08 -0500

    Time: 35 ms (98.35%), Space: 16.3 MB (28.08%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<= 0:
+            return False
+    
+       
+        while n > 1:
+            n = n/2
+        
+        if n==1:
+            return True
+        return False
+        
