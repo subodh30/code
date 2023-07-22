2ff8480947f9e0aa61c5b04f6a2dfd3a1862c6a6
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-21 21:05:53 -0500

    Time: 50 ms (30.81%), Space: 16.3 MB (69.20%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         arr=[0]*(n+1)
-        if n==1 or n==0:
+        if n < 3:
             return n
-        if n==2:
-            return 2
         arr[1]=1
         arr[2]=2
         for i in range(3, n+1):
