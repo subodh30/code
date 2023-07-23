36c2a09aa823da54f8f8a9267e210390d86ccebd
Subodh Gujar <subodhsgujar@gmail.com>
2023-07-23 16:06:47 -0500

    Time: 978 ms (16.67%), Space: 32.1 MB (16.67%) - LeetHub

---
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxArrayValue(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i-1] <= nums[i]:
+                nums[i-1] += nums[i]
+                
+        return max(nums)
