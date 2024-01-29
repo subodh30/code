3a87e44c42463e4dfbb6890c042ce70c73e509bd
Subodh Gujar <subodhsgujar@gmail.com>
2024-01-29 14:02:28 -0500

    Time: 99 ms (38.37%), Space: 21.2 MB (72.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,20 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        sdict = {}
-        for x in nums:
-            if x not in sdict:
-                sdict[x] = 0
-            sdict[x]+=1
+    def topKFrequent(self, nums: List[int], K: int) -> List[int]:
+        d = {}
+        for n in nums:
+            if d.get(n,None) == None:
+                d[n] = 0
+            d[n] += 1
+        
+        h = []
+        for k, v in d.items():
+            h.append((-v, k))
             
-        sdict=dict(sorted(sdict.items(), key= lambda x: x[1], reverse=True))
-        return list(sdict.keys())[:k]
+        heapq.heapify(h)
+        ans = []
+        i = 0
+        while i < K:
+            v, k = heapq.heappop(h)
+            ans.append(k)
+            i+=1
+        return ans
