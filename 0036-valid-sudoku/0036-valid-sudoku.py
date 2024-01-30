97236cf54e78a92f86550a8f227b260482f77718
Subodh Gujar <subodhsgujar@gmail.com>
2024-01-30 09:17:19 -0500

    Time: 90 ms (80.85%), Space: 16.7 MB (59.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,61 +1,45 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def checkColumn(board: List[List[str]]) -> bool:
-            for i in range(9):
-                sdict = {}
-                for j in range(9):
-                    if board[j][i] != ".":
-                        if sdict.get(board[j][i]) == None:
-                            sdict[board[j][i]] = 1
-                        else:
-                            return False
-            return True
-        
-        def checkRow(board: List[List[str]]) -> bool:
-            for i in range(9):
-                sdict = {}
-                for j in range(9):
-                    if board[i][j] != ".":
-                        if sdict.get(board[i][j]) == None:
-                            sdict[board[i][j]] = 1
-                        else:
-                            return False
-            return True
-        
-        def checkBox(board: List[List[str]]) -> bool:
-            for i in range(3):
-                for j in range(3):
-                    sdict={}
-                    for k in range(3):
-                        for l in range(3):
-                            x = i*3 + k
-                            y = j*3 + l
-                            if board[x][y] != ".":
-                                if sdict.get(board[x][y]) == None:
-                                    sdict[board[x][y]] = 1
-                                else:
-                                    return False
-            return True
-       
+    def isValidSudoku(self, b: List[List[str]]) -> bool:
+        rows={}
+        cols={}
+        mat={}
+        for x in range(9):
+            rows[x]=set()
+            cols[x]=set()
 
-        return checkColumn(board) and checkRow(board) and checkBox(board)
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
+        for i in range(3):
+            for j in range(3):
+                mat[(i,j)] = set()
                 
+        def getMat(i,j):
+            x,y = -1,-1
+            if i >= 0 and i <= 2:
+                x = 0
+            elif i >= 3 and i <= 5:
+                x = 1
+            else:
+                x = 2
+                
+            if j >= 0 and j <= 2:
+                y = 0
+            elif j >= 3 and j <= 5:
+                y = 1
+            else:
+                y = 2
+            return (x,y)
+            
+        for i in range(9):
+            for j in range(9):
+                if b[i][j]==".":
+                    continue
+                
+                x,y = getMat(i,j)
+                if b[i][j] in rows[i] or b[i][j] in cols[j] or b[i][j] in mat[(x,y)]:
+                    return False
+                
+                rows[i].add(b[i][j])
+                cols[j].add(b[i][j])
+                mat[(x,y)].add(b[i][j])
+                
+        return True
+        
