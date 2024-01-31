0464573b2d982a3469e5f01e78ba607454faf5ba
Subodh Gujar <subodhsgujar@gmail.com>
2024-01-31 11:18:35 -0500

    Time: 121 ms (32.02%), Space: 18.8 MB (60.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,23 @@
 class Solution:
-    def trap(self, h: List[int]) -> int:
-        l,r = 0, len(h)-1
-        ans=0
-        maxL, maxR = h[l], h[r]
-        while l < r:
-            if maxL <= maxR:
-                l += 1
-                a = maxL - h[l]
-                if a > 0:
-                    ans += a
-                maxL = max(maxL, h[l])
-            else:
-                r -= 1
-                a = maxR - h[r]
-                if a > 0:
-                    ans+=a
-                maxR = max(maxR, h[r])
+    def trap(self, height: List[int]) -> int:
+        if len(height) <= 2:
+            return 0
+        maxL = [0 for _ in range(len(height))]
+        maxL[1] = max(height[0], maxL[0])
+        for i in range(2, len(height)):
+            maxL[i] = max(maxL[i-1], height[i-1])
+        height = height[::-1]
+        maxR = [0 for _ in range(len(height))]
+        maxR[1] = max(height[0], maxR[0])
+        for i in range(2, len(height)):
+            maxR[i] = max(maxR[i-1], height[i-1])
+        maxR = maxR[::-1]
+        height = height[::-1]
+
+        ans = 0
+        for i, h in enumerate(height):
+            ans += max(0, min(maxL[i], maxR[i]) - h)
+            
         return ans
                 
                 
