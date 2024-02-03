e1b98695080a96ec7aefdf60d5069a74f2b95beb
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-03 10:07:29 -0500

    Time: 261 ms (85.22%), Space: 18.2 MB (68.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,27 +1,20 @@
 class Solution:
-    def minEatingSpeed(self, p: List[int], h: int) -> int:
-        def check(k):
-            nonlocal p
-            nonlocal h
-            ans = 0
-            for x in p:
-                ans += (x//k)
-                if x%k != 0:
-                    ans+=1
-            
-            if ans <= h:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        def finish(k):
+            hr = 0
+            for x in piles:
+                hr += math.ceil(x/k)
+            if hr <= h:
                 return True
             return False
-        
-        l,r = 1, 10**9 + 7
-        ans = 0
+        ans = max(piles)
         while l <= r:
-            mid = (l+r)//2
-            if check(mid):
-                ans = mid
-                r = mid - 1
+            m = (l+r)//2
+            if finish(m):
+                ans = min(m, ans)
+                r = m-1
             else:
-                l = mid + 1
+                l = m + 1
+                
         return ans
-        
-        
