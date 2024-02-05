f5517926b1013504615eb94953dbddd3eacccb6c
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-05 13:45:58 -0500

    Time: 57 ms (65.18%), Space: 20.4 MB (85.91%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,60 +7,39 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        d ={}
-        def findP(root):
-            nonlocal d
-            nonlocal p
-            if root == None:
-                return False
-            if root == p:
-                d[root.val] = root
-                return True
+        s = set()
+        def findP(node):
+            if not node:
+                return
             
-            if findP(root.left):
-                d[root.val] = root
+            if node == p:
+                s.add(node)
                 return True
-            
-            if findP(root.right):
-                d[root.val] = root
+            if findP(node.left) or findP(node.right):
+                s.add(node)
                 return True
             return False
-        
+        ans = None
         findP(root)
-        # print(d.keys())
-        def getLCA(root):
-            nonlocal d
-            nonlocal q
+        def findLCA(node):
+            nonlocal ans
+            if node == None:
+                return False
             
-            if root == None:
-                return None, False
+            if node == q:
+                if node in s:
+                    ans = node
+                return True
             
-            if root.val == q.val:
-                if d.get(root.val, None) == None:
-                    return None, True
-                else:
-                    return root, True
+            if findLCA(node.left) or findLCA(node.right):
+                if ans == None and node in s:
+                    ans = node
+                return True
+            return False
                 
-            lr, l = getLCA(root.left)
-            if l and lr==None:
-                 if d.get(root.val, None) == None:
-                    return None, True
-                 else:
-                    return root, True
-            elif l:
-                return lr, l
+        findLCA(root)
+        return ans
                 
-            rr, r = getLCA(root.right)
-            if r and rr==None:
-                 if d.get(root.val, None) == None:
-                    return None, True
-                 else:
-                    return root, True
-            elif r:
-                return rr, r
-            return None, False
+                
+            
         
-        return getLCA(root)[0]
-            
-                
-                
