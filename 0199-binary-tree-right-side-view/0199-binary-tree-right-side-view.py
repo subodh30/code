ccddd4aaae443c137e3d58ad9f86186ca1925a0e
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-05 13:55:42 -0500

    Time: 39 ms (52.65%), Space: 16.6 MB (65.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,17 +6,14 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        lvl=[]
-        def order(root, i):
-            nonlocal lvl
-            if root==None:
-                return
-            if len(lvl) <= i:
-                lvl.append(root.val)
-            order(root.left, i+1)
-            lvl[i]=root.val
-            order(root.right, i+1)
-        
-        order(root, 0)
-        return lvl
-        
+        ans = []
+        def travel(i, node):
+            if node== None:
+                return 
+            if i >= len(ans):
+                ans.append(node.val)
+            ans[i] = node.val
+            travel(i+1, node.left)
+            travel(i+1, node.right)
+        travel(0, root)
+        return ans
