b54d4b9d6efc8e460f0fb219afac240162806192
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-07 11:24:38 -0500

    Time: 1075 ms (95.45%), Space: 51 MB (94.40%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,35 +1,35 @@
 class WordDictionary:
 
     def __init__(self):
-        self.d = {}
+        self.d={}
 
-    def addWord(self, w: str) -> None:
-        tri = self.d
-        for char in w:
-            if char not in tri:
-                tri[char] = {}
-            tri = tri[char]
-        tri["END"] = {}
-            
+    def addWord(self, word: str) -> None:
+        tmp = self.d
+        for w in word:
+            if w not in tmp:
+                tmp[w]={}
+            tmp = tmp[w]
+        if "end" not in tmp:
+            tmp["end"] = ""
+    
     def search(self, word: str) -> bool:
-        def dfs(word, curr, idx):
-            if idx < len(word):
-                if word[idx] == ".":
-                    for key in curr:
-                        if dfs(word, curr[key], idx+1):
+        def find(word, t):
+            for i, w in enumerate(word):
+                if w == ".":
+                    for letter in t:
+                        if "end" != letter and find(word[i+1:], t[letter]):
                             return True
                     return False
-                if word[idx] not in curr:
-                    return False
-            if idx == len(word):
-                if "END" in curr:
-                    return True
                 else:
-                    return False
-            curr = curr[word[idx]]
-            return dfs(word, curr, idx+1)
-        return dfs(word, self.d, 0)
+                    if w not in t:
+                        return False
+                    t = t[w]
+            if "end" in t:
+                return True
+            return False
         
+        return find(word, self.d)
+                    
 
 
 # Your WordDictionary object will be instantiated and called as such:
