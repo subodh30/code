e4e464d7b203f458b5b3bf0ddd8a4f15da450546
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-11 13:50:57 -0500

    Time: 7948 ms (14.32%), Space: 17.9 MB (89.26%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,59 +1,47 @@
-class Trie:
-    def __init__(self):
-        self.child={}
-        self.end=False
-
 class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def getTrie(words):
-            def addWord(word, d):
-                temp = d
-                for c in word:
-                    if c not in temp.child:
-                        temp.child[c] = Trie()
-                    temp = temp.child[c]
-                temp.end=True
-            tri = Trie()
-            for word in words:
-                addWord(word, tri)
-            return tri
-            
-        tri = getTrie(words)
-        m = len(board)
-        n = len(board[0])
+    def findWords(self, b: List[List[str]], words: List[str]) -> List[str]:
+        t = {}
+        m,n = len(b)-1, len(b[0])-1
+        for word in words:
+            tmp = t
+            for w in word:
+                if w not in tmp:
+                    tmp[w] = {}
+                tmp = tmp[w]
+            if "end" not in tmp:
+                tmp["end"]=""
         
-        def dfs(tri, i, j, ans, curWord):
-            nonlocal m , n
-            if tri.end:
-                ans.append(curWord)
-                tri.end=False
-            
-            if board[i][j] in tri.child:
-                c = board[i][j]
-                cc = curWord + board[i][j]
-                board[i][j]=None
-                if i > -1 and i+1 < m and j > -1 and j < n:
-                    dfs(tri.child[c], i+1, j, ans, cc)
-                    
-                if i > -1 and i < m and j > -1 and j+1 < n:
-                    dfs(tri.child[c], i, j+1, ans, cc)
-                    
-                if i-1 > -1 and i < m and j > -1 and j < n:
-                    dfs(tri.child[c], i-1, j, ans, cc)
-                    
-                if i > -1 and i < m and j-1 > -1 and j < n:
-                    dfs(tri.child[c], i, j-1, ans, cc)
-                
-                board[i][j]=c
-                if tri.child[c].end:
-                    ans.append(cc)
-                    tri.child[c].end=False
-            
-                    
-        ans=[]
-        for i in range(m):
-                for j in range(n):
-                    dfs(tri, i, j, ans, "")
+        # print(t)
+        d = [(1,0), (0, 1), (-1, 0), (0, -1)]
+        ans = set()
+        vis = set()
+        def dsf(i,j, tmp, word, vis):
+            # print(word)
+            nonlocal ans
+            if "end" in tmp:
+                ans.add(word)
+            if i < 0 or i > m or j < 0 or j > n or (i,j) in vis:
+                return
+            # print(b[i][j])
+            if b[i][j] in tmp:
+                tmp = tmp[b[i][j]]
+                word+=b[i][j]
+                if "end" in tmp:
+                    ans.add(word)
+                vis.add((i, j))  
+                for x,y in d:
+                    ni, nj = i+x, j+y
+                    # print(str(ni) + " " + str(nj))
+                    dsf(ni,nj, tmp, word, vis)
+                vis.remove((i,j))
+            return
+        
+        for i in range(m+1):
+            for j in range(n+1):
+                vis = set()
+                dsf(i,j, t, "", vis)
+        # dsf(2,3, t, "", vis)
         return ans
+                    
             
-       
+        
