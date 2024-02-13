9d38f8a94b4452163f55fa5fea8400f87425921b
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-13 17:05:45 -0500

    Time: 99 ms (91.59%), Space: 16.6 MB (97.81%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,35 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m,n = len(grid), len(grid[0])
-        marea=0
-        def rec(i, j):
-            nonlocal m,n, marea
-            q=[]
-            q.append((i, j))
-            grid[i][j]=0
-            area=0
+        m,n = len(grid)-1, len(grid[0])-1
+        ans = 0
+        def iterate(i,j):
+            nonlocal ans
+            q = [[i,j]]
+            d = [[0,1], [0,-1], [1,0], [-1,0]]
+            area = 0
             while q:
-                ii, jj = q.pop(0)
-                area+=1
-                if ii+1<m and grid[ii+1][jj]:
-                    grid[ii+1][jj]=0
-                    q.append((ii+1, jj))
-                if jj+1<n and grid[ii][jj+1]:
-                    grid[ii][jj+1]=0
-                    q.append((ii, jj+1))
-                if ii-1>=0 and grid[ii-1][jj]:
-                    grid[ii-1][jj]=0
-                    q.append((ii-1, jj))
-                if jj-1 >=0  and grid[ii][jj-1]:
-                    grid[ii][jj-1]=0
-                    q.append((ii, jj-1))
-            marea = max(marea, area)
-            
-            
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]:
-                    rec(i, j)
+                # print(q)
+                x, y = q.pop(0)
+                if grid[x][y] == 1:
+                    area+=1
+                    grid[x][y] = 0
+                    for ii, jj in d:
+                        xx, yy = x+ii, y+jj
+                        if not (xx<0 or xx >m or yy <0 or yy > n):
+                            if grid[xx][yy] == 1:
+                                q.append([xx, yy])
+            ans = max(ans, area)
         
-        return marea
-        
+        for i in range(m+1):
+            for j in range(n+1):
+                if grid[i][j] == 1:
+                    iterate(i,j)
+        # print(grid)
+        return ans
