4ea5f130c2c545ff01b21454ee24ce48a4862a26
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-13 10:13:24 -0500

    Time: 36 ms (87.10%), Space: 16.9 MB (53.35%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def rec(ans, arr):
-            nonlocal nums
-            if len(arr)==len(nums):
-                ans.append(arr.copy())
-                return
-            for n in nums:
-                if n not in arr:
-                    arr.append(n)
-                    rec(ans, arr)
-                    arr.pop()
-                    
-        ans=[]
-        rec(ans, [])
-        return ans
-            
+        ans = []
         
+        def test(temp):
+            if len(temp) == len(nums):
+                ans.append(temp.copy())
+                return
+            
+            for x in nums:
+                if x not in temp:
+                    temp.append(x)
+                    test(temp)
+                    temp.pop()
+                  
+        test([])
+        return ans
