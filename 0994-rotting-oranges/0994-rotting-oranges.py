5628412695a206c1cd5a7e218f1919d403d5964b
Subodh Gujar <subodhsgujar@gmail.com>
2024-02-28 10:56:52 -0500

    Time: 59 ms (19.47%), Space: 16.8 MB (36.73%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,47 +1,38 @@
 class Solution:
-    def orangesRotting(self, g: List[List[int]]) -> int:
-        m,n = len(g), len(g[0])
-        tt = [[float(inf) for _ in range(n)] for _ in range(m)]
-        def rec(i, j, vis):
-            nonlocal m,n,tt
-            q=[]
-            q.append((i,j, 0))
-            vis[i][j]=True
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m,n = len(grid), len(grid[0])
+        def bfs(i,j):
+            q=[[i,j,0]]
+            vis = set()
             while q:
-                ii, jj, time = q.pop(0)
-                tt[ii][jj] = min(tt[ii][jj], time)
-                if ii+1<m and not vis[ii+1][jj] and g[ii+1][jj]==1:
-                    vis[ii+1][jj]=True
-                    q.append((ii+1, jj, time+1))
-                    
-                if ii-1>=0 and not vis[ii-1][jj] and g[ii-1][jj]==1:
-                    vis[ii-1][jj]=True
-                    q.append((ii-1, jj, time+1))
+                # print(q)
+                x,y,t = q.pop(0)
+                if (x,y) in vis:
+                    continue
+                vis.add((x,y))
+                t+=1
                 
-                if jj+1<n and not vis[ii][jj+1] and g[ii][jj+1]==1:
-                    vis[ii][jj+1]=True
-                    q.append((ii, jj+1, time+1))
+                d = [[1,0], [0,1], [-1,0], [0,-1]]
+                for ii, jj in d:
+                    xx, yy = x+ii, y+jj
+                    if xx < 0 or xx >= m or yy < 0 or yy >= n or (xx, yy) in vis:
+                        continue
                     
-                if jj-1>=0 and not vis[ii][jj-1] and g[ii][jj-1]==1:
-                    vis[ii][jj-1]=True
-                    q.append((ii, jj-1, time+1))
-                            
+                    if grid[xx][yy] == 2:
+                        return t
+                    if grid[xx][yy] == 1:
+                        q.append([xx, yy, t])
+            return -1
         
+        ans = 0
+        # return bfs(1,1)
         for i in range(m):
             for j in range(n):
-                if g[i][j]==2:
-                    vis = [[False for _ in range(n)] for _ in range(m)]
-                    rec(i,j, vis)
+                if grid[i][j] == 1:
+                    temp = bfs(i,j)
+                    if temp == -1:
+                        return -1
+                    ans = max(ans, temp)
+                    print("{} {} {}".format(i, j, temp))
                     
-        for i in range(m):
-            for j in range(n):
-                if g[i][j]==1 and tt[i][j] == float("inf"):
-                    return -1
-                
-        mt = 0
-        for i in range(m):
-            for j in range(n):
-                if tt[i][j] != float("inf"):
-                    mt = max(mt, tt[i][j])
-        return mt
-        
+        return ans
