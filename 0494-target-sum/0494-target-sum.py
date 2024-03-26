8af5df99f409a01f4e6976e85f8e803e5c0a353e
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-26 11:32:51 -0400

    Time: 224 ms (32.03%), Space: 37.8 MB (35.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,24 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        ans = 0
-        memo={}
-        def rec(i, ss):
-            nonlocal ans, memo
-            if i == len(nums):
-                if ss == target:
-                    return 1
-                else:
-                    return 0
-            if (i, ss) in memo:
-                return memo[(i, ss)]
-            memo[(i, ss)] = rec(i+1, ss+nums[i]) + rec(i+1, ss-nums[i])
-            return memo[(i, ss)]
+        memo = {}
+        def getWays(i, s):
+            # print(str(i) + " " + str(s))
+            if s == target and i == len(nums):
+                # print("res")
+                return 1
+            
+            if i >= len(nums):
+                return 0
+            
+            if (i, s) in memo:
+                return memo[(i,s)]
+            
+            tot = getWays(i+1, s + nums[i])
+            tot += getWays(i+1, s - nums[i])
+            
+            memo[(i, s)] = tot
+            return tot
         
-        return rec(0, 0)
+        ans = getWays(0, 0)
+        # print(memo)
+        return ans
