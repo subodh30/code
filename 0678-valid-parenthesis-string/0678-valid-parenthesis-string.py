0f68d0a1f4dbec48d6f6f0b66ef2349b451e4e6d
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-27 14:19:34 -0400

    Time: 46 ms (9.54%), Space: 16.6 MB (50.73%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,30 +1,16 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        dp = {}
-        def dfs(i, lc):
-            if i >= len(s) and lc ==0:
-                return True
-            if i >= len(s):
-                return False
+        openMin, openMax = 0,0
+        for i in s:
+            if i == '(':
+                openMin, openMax = openMin + 1, openMax + 1
+            elif i == ')':
+                openMin, openMax = max(openMin-1, 0), openMax - 1
+            else:
+                openMin, openMax = max(openMin - 1, 0), openMax + 1
+            if openMax < 0:
+                return  False
+        return openMin == 0
             
-            if lc < 0:
-                return False
-            
-            key = (i, lc)
-            if key in dp:
-                return dp[key]
-            
-            if s[i] == "(":
-                dp[key] = dfs(i+1, lc+1)
-                return dp[key]
-            
-            if s[i] == ")":
-                dp[key] = dfs(i+1, lc - 1)
-                return dp[key]
-            
-            dp[key] = dfs(i+1, lc) or dfs(i+1, lc+1) or dfs(i+1, lc-1)
-            return dp[key]
         
         
-        return dfs(0, 0)
-            
