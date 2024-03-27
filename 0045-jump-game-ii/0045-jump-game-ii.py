2f419d181454168896b5a96dfdbfef3ac3bf0dd2
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-27 11:13:05 -0400

    Time: 108 ms (56.24%), Space: 17.4 MB (96.67%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,28 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
-        cur = mx = 0
-        ret = 0
-        while mx < len(nums)-1:
-            ans = 0
-            for i in range(cur, mx+1):
-                ans = max(ans, i + nums[i])
-            cur = mx+1
-            mx = ans
+        l = r = 0
+        longest = ret  = 0
+        while r < len(nums)-1:
+            for i in range(l, r+1):
+                longest = max(longest, i + nums[i])
+            l = r+1
+            r = longest
             ret+=1
         return ret
-            
         
-#         reach = [float("inf") for _ in range(len(nums))]
-#         reach[0] = 0
-#         for i in range(1, len(nums)):
-                
-#             j = i-1
-#             while j >=0:
-#                 if reach[j] != float("inf") and i-j <= 1000 and i - j <= nums[j]:
-#                     reach[i] = min(reach[j]+1, reach[i])
-                    
-#                 j-=1
-#         # print(reach)
-#         return reach[len(nums)-1]
-
