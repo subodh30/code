b01571a8db21fa6f5a74b2211faf385f04222602
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-27 11:55:52 -0400

    Time: 270 ms (18.49%), Space: 18.1 MB (87.88%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,19 +1,15 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand)%groupSize != 0:
+        if len(hand) % groupSize != 0:
             return False
         
-            
-        nums = sorted(hand)
-        while nums:
-            # print(nums)
-            i = nums[0]
-            g = groupSize
-            while g > 0:
-                if i in nums:
-                    nums.remove(i)
+        hand = sorted(hand)
+        while hand:
+            h = hand[0]
+            # print(hand)
+            for i in range(groupSize):
+                if h+i in hand:
+                    hand.remove(h+i)
                 else:
                     return False
-                g-=1
-                i+=1
         return True
