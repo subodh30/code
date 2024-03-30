9fc5b659c92985670e17b672a8008f75e22481f7
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-30 15:34:34 -0400

    Time: 113 ms (12.51%), Space: 16.5 MB (62.12%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -2,18 +2,24 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if "0" in [num1, num2]:
             return "0"
-
-        res = [0] * (len(num1) + len(num2))
-        num1, num2 = num1[::-1], num2[::-1]
-        for i1 in range(len(num1)):
-            for i2 in range(len(num2)):
-                digit = int(num1[i1]) * int(num2[i2])
-                res[i1 + i2] += digit
-                res[i1 + i2 + 1] += res[i1 + i2] // 10
-                res[i1 + i2] = res[i1 + i2] % 10
-
-        res, beg = res[::-1], 0
-        while beg < len(res) and res[beg] == 0:
-            beg += 1
-        res = map(str, res[beg:])
-        return "".join(res)
+        ans = [0] * (len(num1) + len(num2))
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        for i, n1 in enumerate(num1):
+            for j, n2 in enumerate(num2):
+                temp = int(n1) * int(n2)
+                ans[i+j] += temp % 10
+                c = 0
+                if ans[i+j] > 9:
+                    c = ans[i+j] // 10
+                    ans[i+j] = ans[i+j] % 10
+                ans[i+j+1] += (temp//10 + c)
+        ret = ""
+        zero = True
+        # print(ans)
+        for x in ans[::-1]:
+            if zero and x == 0:
+                continue
+            zero = False
+            ret += str(x)
+        return ret
