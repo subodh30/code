183af6ba0335d7e30ece712caf0d1c2fad9ebcbc
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-30 11:29:40 -0400

    Time: 35 ms (69.88%), Space: 16.5 MB (84.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,15 @@
 class Solution:
-    def plusOne(self, d: List[int]) -> List[int]:
-        ans = []
-        c=1
-        for i in range(len(d)-1, -1, -1):
-            t = d[i]+c
-            c = t // 10
-            ans.append(t % 10)
-            
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
         
-        if c==1:
-            ans.append(1)
-        return ans[::-1]
-        
+        digits.insert(0, 1)
+        return digits
             
