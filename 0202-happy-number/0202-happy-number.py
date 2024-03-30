776c2f51171ad0b9f8ac779f9b6808bd5e267694
Subodh Gujar <subodhsgujar@gmail.com>
2024-03-30 11:16:57 -0400

    Time: 35 ms (71.46%), Space: 16.5 MB (81.10%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        while True:
-            ans = 0
-            while n!=0:
-                m = n %10
-                n = n//10
-                ans+=m*m
-            if len(str(ans)) == 1:
-                return ans == 1 or ans == 7
-            n = ans
-        
+        def getSumOfSq(x):
+            temp = 0
+            for s in x:
+                temp += int(s)**2
+            return str(temp)
+        ans=getSumOfSq(str(n))
+        while len(ans) > 1:
+            ans = getSumOfSq(ans)
+            
+        return ans == "1" or ans == "7"
