279f9c8f0413bfe9a04277a5674ad822993013b7
Subodh Gujar <subodhsgujar@gmail.com>
2024-04-12 17:33:47 -0400

    Time: 30 ms (99.32%), Space: 17.5 MB (65.04%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -12,20 +12,39 @@ class Solution:
     def connect(self, root: 'Node') -> 'Node':
         if not root:
             return None
-        d = {}
-        def dfs(node, lvl):
-            nonlocal d
-            if not node:
-                return 
-            if lvl not in d:
-                d[lvl] = []
-            d[lvl].append(node)
-            dfs(node.left, lvl+1)
-            dfs(node.right, lvl+1)
-        
-        dfs(root, 0)
-        for v in d.values():
-            for i, node in enumerate(v[:-1]):
-                node.next = v[i+1]
+        q, q1=[root], []
+        cur, prev = None, None
+        while q or q1:
+            if q:
+                cur = q.pop(0)
+                if prev:
+                    prev.next = cur
+                if cur.left:
+                    q1.append(cur.left)
+                if cur.right:
+                    q1.append(cur.right)
+                prev = cur
+                
+            else:
+                q = q1.copy()
+                q1=[]
+                prev = None
         return root
+        
+#         d = {}
+#         def dfs(node, lvl):
+#             nonlocal d
+#             if not node:
+#                 return 
+#             if lvl not in d:
+#                 d[lvl] = []
+#             d[lvl].append(node)
+#             dfs(node.left, lvl+1)
+#             dfs(node.right, lvl+1)
+        
+#         dfs(root, 0)
+#         for v in d.values():
+#             for i, node in enumerate(v[:-1]):
+#                 node.next = v[i+1]
+#         return root
                 
